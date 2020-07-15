0c6aaa560a58fe5dd561d7aa551789f4c214a9f3
Shreya Swaroop <shreyaswrp@gmail.com>
2020-07-15 17:46:39 +0530

    Unit conversion

--- Unit_Conversion.sh
+++ Unit_Conversion.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -x
 
-#read -p "Enter your number" inches
-#feet=0
-#feet=$(($inches / 12))
-#echo $feet 
+	read -p "Enter your number" inches
 
-length=60
-breadth=40
+	feet=0
+	feet=$(($inches / 12))
+	echo $feet 
 
-meters1=`echo $length* 0.3048 |bc`;
-meters2=`echo $breadth* 0.3048 |bc`;
+	length=60
+	breadth=40
 
-area=`echo $meters1*$meters2 |bc`;
-echo $area
+	meters1=`echo $length* 0.3048 |bc`;
+	meters2=`echo $breadth* 0.3048 |bc`;
 
-area25=`echo $area*25 |bc`;
-area25acres=`echo $area25*0.00025 |bc`;
-echo $area25acres
+	#Area of Rectengular Plot
+	area=`echo $meters1*$meters2 |bc`;
+	echo $area
+
+	#Area of 25 such plots in acres
+	area25=`echo $area*25 |bc`;
+	area25acres=`echo $area25*0.00025 |bc`;
+	echo $area25acres
